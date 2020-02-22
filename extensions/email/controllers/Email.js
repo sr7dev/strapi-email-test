87a076cccbacdba65dc5e22fa6369824d11f3e47
white <whitecalm@outlook.com>
2020-02-22 19:48:20 +0300

    add and update /extension/controllers/email.js

--- extensions/email/controllers/Email.js
+++ extensions/email/controllers/Email.js
@@ -2,16 +2,16 @@
 
 module.exports = {
   send: async (ctx) => {
-    const {email} = ctx.request.body;
+    const body= ctx.request.body;
     console.log(ctx.request.body);
     try {
       var info = await strapi.plugins.email.services.email.send({
-              to: 'j.perfectsolution55@gmail.com',
-              from: 'your@email.address',
-              replyTo: 'your@email.address',
-              subject: 'It is my message',
-              text: 'Coucou',
-              html: 'Coucou'
+              to: 'j.perfectsolution55@gmail.com', // Please type your personal or business email
+              from: body.name + ` <${body.email}>`,
+              replyTo: 'no-reply@me.com',
+              subject: 'Hello',
+              text: `This is ${body.name}`,
+              html: body.message
             });
       console.log(info);
       ctx.send({ status: "success", message: "successfully!!!", data: info });
